c187d49326c199827e12605ef6ebda60674d94cb
Tytan <thomasnardou@icloud.com>
2024-09-19 16:00:50 +0200

    add request to the API

---
+++ public/js/test.js
@@ -0,0 +1,11 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.getElementById('btn').addEventListener('click', function() {
+//         $.ajax({
+//             url: '/test-get',
+//             type: 'GET',
+//             success: function(response) {
+//                 console.log(response);
+//             }
+//         })
+//     });
+// })
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test-get', function () {
+    return response()->json([
+        'message' => 'Hello, World!',
+        'test' => 'How are you?',
+        'test2' => 'I am fine, thank you.'
+    ]);
+});
